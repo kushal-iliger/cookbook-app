fc44e7e6a4aaff96713d59951e93db856d0efdb0
kushal-iliger <k.jagadeesh-iliger@hellogetsafe.com>
2024-02-26 13:34:59 +0100

    outline of the project with all the components

--- src/components/recipes/reciepe-list/reciepe-list.component.ts
+++ src/components/recipes/reciepe-list/reciepe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-reciepe-list',
   templateUrl: './reciepe-list.component.html',
   styleUrl: './reciepe-list.component.css'
 })
-export class ReciepeListComponent {
+export class ReciepeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test', 'Testing', 'https://firebasestorage.googleapis.com/v0/b/portfolio-kushal-ij.appspot.com/o/prateek-katyal-xv7-GlvBLFw-unsplash.jpg?alt=media&token=8406ed62-2385-43dc-8210-0d1e32d28994'),
+    new Recipe('Test', 'Testing', 'https://firebasestorage.googleapis.com/v0/b/portfolio-kushal-ij.appspot.com/o/prateek-katyal-xv7-GlvBLFw-unsplash.jpg?alt=media&token=8406ed62-2385-43dc-8210-0d1e32d28994')
+  ];
 
+  constructor() { }
+  ngOnInit(): void {
+
+  }
 }
--- src/components/shopping-list/shopping-list.component.ts
+++ src/components/shopping-list/shopping-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../app/shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
 
+  ingredients: Ingredient[] = [
+    new Ingredient('Banana', 5),
+    new Ingredient('Apple', 7)
+  ];
+
+  constructor() { }
+  ngOnInit(): void {
+
+  }
 }
